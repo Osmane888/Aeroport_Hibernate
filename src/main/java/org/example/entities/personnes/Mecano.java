d1af865870adfc5da65cbf72416e3b2d12aca114
osmane <osmane688@hotmail.com>
2024-09-22 21:34:45 +0200

    kfpiojpzifj

--- src/main/java/org/example/entities/personnes/Mecano.java
+++ src/main/java/org/example/entities/personnes/Mecano.java
@@ -1,7 +1,18 @@
 package org.example.entities.personnes;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import org.example.entities.machines.Avion;
 
 @Entity
 public class Mecano extends Personne{
+
+    @ManyToMany
+    @JoinTable(
+            name = "Maintenance",
+            joinColumns = @JoinColumn(name = "id"),
+            inverseJoinColumns = @JoinColumn(name = "immatriculation"))
+    private Avion avion;
 }
